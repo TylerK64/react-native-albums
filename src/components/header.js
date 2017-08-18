7c6a93054eec47277e35d5d856eb3c0ac746a236
Tyler Kawabata <tylerkawabata@gmail.com>
2017-08-18 12:27:02 -0700

    Add src folder for jsx files and add styling for Header component.

---
+++ src/components/header.js
@@ -0,0 +1,11 @@
+//import libraries for making a Component
+import React from 'react';
+import { Text } from 'react-native';
+
+//make a component
+const Header = () => {
+  return <Text>Albums</Text>;
+};
+
+//make a component for other parts of the app
+export default Header;
